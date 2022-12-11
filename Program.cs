a38482c82a0abf625e30ac91499ad78b65308777
Ros Finkov <Ros.Finkov@gmail.com>
2022-12-11 18:41:21 +0200

    HeapSort Algo

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+static void HeapSort(int[] arr) {
+	int heapSize = arr.Length;
+
+	for (int i = arr.Length / 2 - 1; i >= 0; i--)
+		Heapify(arr, heapSize, i);
+
+	for (int i = arr.Length - 1; i > 0; i--)
+	{
+		int temp = arr[i];
+		arr[i] = arr[0];
+		arr[0] = temp;
+
+		Heapify(arr, i, 0);
+	}
+}
+
+static void Heapify(int[] arr, int heapSize, int index) {
+    int left = (index + 1) * 2 - 1;
+    int right = (index + 1) * 2;
+    int largest = index;
+
+    if (left < heapSize && arr[left] > arr[index])
+		largest = left;
+	else
+		largest = index;
+
+	if (right < heapSize && arr[right] > arr[largest])
+		largest = right;
+
+	if (largest != index)
+	{
+		int temp = arr[index];
+		arr[index] = arr[largest];
+		arr[largest] = temp;
+
+		Heapify(arr, heapSize, largest);
+	}
+}
+
+
+
+int[] arr = new int[] { 1, 13, -2, 43, -17 };
+HeapSort(arr);
+
+foreach (int num in arr) {
+	Console.WriteLine(num);
+}
