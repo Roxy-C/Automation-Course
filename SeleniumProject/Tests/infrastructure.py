d31c8e2d5832fc12ed8fa2d172e7f4cc685c4d5c
Ros Finkov <82277539+Roxy-C@users.noreply.github.com>
2023-02-21 08:27:05 +0200

    SeleniumProject updated

---
+++ SeleniumProject/Tests/infrastructure.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+
+class Infrastructure:        
+    def log(self, message):
+        try:
+            time = datetime.now().strftime(" %d/%m/%Y %H:%M:%S ")
+            os.chdir(os.getenv('LOGLOCATION'))
+            self.file = open(os.getenv('LOGNAME'), 'a')
+            self.file.write(message + "\t" + time + "\n")
+            self.file.flush()
+            self.file.close()
+        except FileNotFoundError:
+            print("Exception occurred: File not found")
